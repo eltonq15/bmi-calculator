852304a0961f56ffe677255ba53f3d2f20215270
Elton Ribeiro <elton.ribeiro-ext@dalenys.com>
2022-08-23 21:50:12 +0100

    add first ads

--- src/components/AdsSection.jsx
+++ src/components/AdsSection.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export const AdsSection = () => {
-  return <section className=''></section>;
+  return (
+    <section className=''>
+      <amp-ad
+        width='100vw'
+        height='320'
+        type='adsense'
+        data-ad-client='ca-pub-4608458965420345'
+        data-ad-slot='8647895919'
+        data-auto-format='rspv'
+        data-full-width=''
+      >
+        <div overflow=''></div>
+      </amp-ad>
+    </section>
+  );
 };
